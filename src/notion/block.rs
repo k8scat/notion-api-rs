488f5133b1a16fe6a731403fd056d59a6c21a9dc
K8sCat <k8scat@gmail.com>
2021-12-14 10:26:05 +0800

    fix: serde struct

--- src/notion/block.rs
+++ src/notion/block.rs
@@ -1,6 +1,8 @@
 use super::client::Client;
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Block {
     pub object: String,
     pub id: String,
@@ -12,20 +14,12 @@ pub struct Block {
 }
 
 impl Client {
-    pub async fn retrieve_block(&self, id: String) -> Result<Block> {
-        let url = format!("{}/api/v3/blocks/{}", self.base_url, id);
-        let response = self.get(&url).await?;
-        let body = response.text().await?;
-        let block: Block = serde_json::from_str(&body)?;
-        Ok(block)
+    pub async fn retrieve_block(&self, id: String) -> Result<()> {
+        Ok(())
     }
 
-    pub async fn update_block(&self, id: String, block: Block) -> Result<Block> {
-        let url = format!("{}/api/v3/blocks/{}", self.base_url, id);
-        let response = self.put(&url, &block).await?;
-        let body = response.text().await?;
-        let block: Block = serde_json::from_str(&body)?;
-        Ok(block)
+    pub async fn update_block(&self, id: String, block: Block) -> Result<()> {
+        Ok(())
     }
 
     pub async fn delete_block(&self, id: String) -> Result<()> {
--- src/notion/search.rs
+++ src/notion/search.rs
@@ -1,6 +1,8 @@
 use super::client::Client;
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SearchParams {
     pub query: String,
     pub start_cursor: String,
@@ -9,11 +11,13 @@ pub struct SearchParams {
     pub filter: Option<SearchFilter>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SearchSort {
     pub direction: String,
     pub timestamp: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SearchFilter {
     pub value: String,
     pub property: String,
@@ -21,12 +25,12 @@ pub struct SearchFilter {
 
 impl Client {
     pub async fn search(&self, params: Option<SearchParams>) -> Result<()> {
-        let url = format!("{}/search", self.base_api);
-        let mut resp = self.client.post(url);
-        if params.is_some() {
-            resp = resp.json(&params);
-        }
-        let resp = resp.send().await?;
+        // let url = format!("{}/search", self.base_api);
+        // let mut resp = self.client.post(url);
+        // if params.is_some() {
+        //     resp = resp.json(&params);
+        // }
+        // let resp = resp.send().await?;
         Ok(())
     }
 }
